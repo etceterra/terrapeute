1c6e8bb8faf020e67dea92b76d6f1b1e0e52e6b3
Vincent Agnano <v.agnano@protonmail.com>
2019-04-04 10:22:37 +0200

    implementing pre-production version in Express with Airtable

--- src/index.mjs
+++ src/index.mjs
@@ -1,26 +1,22 @@
 import express from 'express'
 import nunjucks from 'nunjucks'
+import Airtable from 'airtable'
+import Vcard from 'vcards-js'
+import dotenv from 'dotenv'
 
-import Provider from './models/providers.mjs'
+dotenv.config()
 
-const config = {
-  port: 8080
-}
+const config = process.env
+
 const app = express()
 
-const providers  = [
-  new Provider({
-    slug: 'massotherapeute-geneve/max-antoine-breda',
-    firstname: "Max-Antoine",
-    lastname: "Breda",
-    // photo: "https://espacelingdesante.ch/wp-content/uploads/2016/08/maxantoine2-1.jpg",
-    therapy: 'Massothérapeute',
-    city: "Genève",
-    description: "Je suis massothérapeute sur Genève depuis 2012. Je pratique le massage classique et thérapeutique",
-    agreements: ['asca', 'rme', 'aptn'],
-    latlng: [46.2115, 6.119],
-  })
-]
+Airtable.configure({ endpointUrl: 'https://api.airtable.com', apiKey: config.AIRTABLE_API_KEY })
+const db = Airtable.base('app1Ab6PilgNTXMYp')
+const Provider = db.table('Therapeutes')
+const Symptom = db.table('Symptomes')
+const Therapy = db.table('Therapies')
+let therapies = []
+let symptoms = []
 
 nunjucks.configure(`src/views`, {
     express: app,
@@ -30,13 +26,62 @@ app.set('view engine', 'html')
 app.use(express.static('assets'))
 
 app.get('/', (req, res) => res.render('index', { title: 'Hey', message: 'Hello there!' }))
-app.get('/therapeutes', (req, res) => {
+app.get('/therapeutes', async (req, res) => {
+  const data = await Provider.select().firstPage()
+  const providers = data.map(p => Object.assign({ id: p.id }, p.fields)).map(p => {
+    p.therapies = p.therapies.map(t => therapies[t])
+    p.therapies_name = p.therapies.map(t => t.name)
+    p.name = `${p.firstname} ${p.lastname}`
+    return p
+  })
   res.render('providers', { providers })
 })
 
-app.get('/:slug0/:slug1', (req, res) => {
-  const provider = providers.find(p => p.slug === `${req.params.slug0}/${req.params.slug1}`)
+app.get('/:slug0/:slug1/:id', async (req, res) => {
+  const response = await Provider.find(req.params.id)
+  const provider = Object.assign({}, response.fields, {
+    id: response.id,
+    url: `/${req.params.slug0}/${req.params.slug1}/${req.params.id}`
+  })
   res.render('provider', { provider })
 })
 
-app.listen(config.port, () => console.log(`App running on port ${config.port}!`))
+app.get('/:slug0/:slug1/:id/vcf', async (req, res) => {
+  const response = await Provider.find(req.params.id.split('.')[0])
+  const provider = response.fields
+  provider.id = response.id
+
+  const vcard = Vcard()
+  vcard.firstName = provider.firstname
+  vcard.lastName = provider.lastname
+  vcard.workPhone = provider.phone
+  vcard.workEmail = provider.email
+  if (provider.socials) {
+    vcard.workUrl = provider.socials.website
+    // ignoring social networks, vCard renders 'CHARSET:UTF8' instead.
+    // try {
+    //   vcard.socialUrls['facebook'] = 'https://...'
+    // } catch(e) {}
+  }
+  if (provider.photo) vcard.photo.attachFromUrl(provider.photo[0].url, 'JPEG')
+  vcard.note = provider.timetable
+  // if (provider.therapies) vcard.title = vcard.role = provider.therapies.map(t => therapies[t]).join(', ')
+  vcard.organization = 'Thérapeute chez Terrapeutes.com'
+  vcard.source = req.url
+  vcard.workAddress.label = 'Cabinet'
+  vcard.workAddress.street = provider.street
+  vcard.workAddress.city = provider.city
+  vcard.workAddress.postalCode = provider.zipcode
+  vcard.workAddress.countryRegion = provider.country
+  res.set('Content-Type', `text/vcard; name="${provider.firstname}-${provider.lastname}.vcf"`)
+  res.set('Content-Disposition', `inline; filename="${provider.firstname}-${provider.lastname}.vcf"`)
+  res.send(vcard.getFormattedString())
+})
+
+;(async () => {
+  const therapyData = await Therapy.select().firstPage()
+  therapyData.forEach(t => therapies[t.id] = t.fields)
+  const symptomData = await Symptom.select().firstPage()
+  symptomData.forEach(s => symptoms[s.id] = s.fields)
+  app.listen(config.PORT, () => console.log(`App running on port ${config.PORT}!`))
+})()
