c5d59dde73d392bcae852331a898ee677d6aa8e5
Vincent Agnano <vinyll@protonmail.com>
2020-01-20 01:52:53 +0100

    added h1 h2 h3 to Trix toolbar

---
+++ assets/js/trix.js
@@ -0,0 +1,10 @@
+/**
+ * Extend Trix-editor
+ */
+document.addEventListener('trix-initialize', (e) => {
+  const blockTools = e.target.toolbarElement.querySelector('[data-trix-button-group=block-tools]')
+  ;['H3', 'H2', 'H1'].forEach(t => {
+    Trix.config.blockAttributes[t] = { tagName: t, terminal: true, breakOnReturn: true, group: false }
+    blockTools.insertAdjacentHTML('afterbegin', `<button type="button" class="trix-button" data-trix-attribute="${t}" title="${t}" tabindex="-1">${t}</button>`)
+  })
+})
