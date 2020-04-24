b8f4939b0f8618812346b51ff3b0f1166f4737bb
Vincent Agnano <vinyll@protonmail.com>
2020-04-24 16:23:13 +0200

    extraData patch fix

--- src/api/routes.mjs
+++ src/api/routes.mjs
@@ -13,8 +13,10 @@ export default function (app, prefix = '') {
   app.patch(`${prefix}/therapist/:id`, async (req, res) => {
     const therapist = await Therapist.findById(req.params.id)
     if(!therapist) return res.status(404).send('Therapist not found')
-    therapist.extraData = await req.body
-    await therapist.save()
+    if(req.body.extraData) {
+      therapist.extraData = req.body.extraData
+      await therapist.save()
+    }
     res.setHeader('Content-Type', 'application/json')
     res.send(therapist.toJSON())
   })
