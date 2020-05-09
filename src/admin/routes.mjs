a5465023e5d7ef5a9554db912d3686a184a211b1
Vincent Agnano <vinyll@protonmail.com>
2020-05-09 11:18:25 +0200

    removed outdated module

--- src/admin/routes.mjs
+++
@@ -1,8 +0,0 @@
-import airtable from '../airtable.mjs'
-
-export default function (app, prefix = '') {
-  app.get(`${prefix}/import-airtable`, async (req, res) => {
-    await airtable.transferAll()
-    res.send("Import depuis Airtable terminé.")
-  })
-}
