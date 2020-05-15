bd8a392d775c421a9893d010ecd5c08f9bd05c86
Vincent Agnano <vinyll@protonmail.com>
2020-05-15 21:34:35 +0200

    wip admin with blog articles

--- src/admin.mjs
+++ src/admin.mjs
@@ -3,21 +3,46 @@ import AdminBroExpress from 'admin-bro-expressjs'
 import AdminBroMongoose from 'admin-bro-mongoose'
 
 import { Therapist, Therapy, Symptom, Synonym } from './therapists/models.mjs'
+import { Article } from './articles/models.mjs'
 
 export default function (app, route) {
   AdminBro.registerAdapter(AdminBroMongoose)
   const adminBro = new AdminBro({
     rootPath: route,
     branding: {
-      companyName: 'Terrapeute Admin',
+      companyName: 'Naturapeute Admin',
+    },
+    dashboard: {
+      component: AdminBro.bundle('./my-dashboard-component')
     },
     resources: [
-      { resource: Synonym,
+      {
+        resource: Synonym,
         options: {
           listProperties: ['name', 'words'],
           showProperties: ['name', 'words'],
           editProperties: ['name', 'words'],
           titleProperty: 'name',
+          isVisible: { list: true, filter: true, show: false, edit: true },
+        }
+      },
+      {
+        resource: Article,
+        options: {
+          listProperties: ['title'],
+          showProperties: ['title', 'slug', 'tags', 'body'],
+          titleProperty: 'title',
+          properties: {
+            slug: { isVisible: false },
+            body: {
+              type: 'richtext'
+            },
+            tags: {
+              components: {
+                // list: AdminBro.bundle('./strings-list.jsx'),
+              }
+            }
+          }
         }
       },
     ],
